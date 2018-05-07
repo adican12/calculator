214651a994d085991e731bcc9a14104247fbcf94
Adican12 <adican12@gmail.com>
2018-05-07 11:12:44 +0300

    not ready

--- app/src/main/java/com/adidroid/calculator/MainActivity.java
+++ app/src/main/java/com/adidroid/calculator/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     final static ArrayList<MyListItem> hw = new ArrayList<>();
     static {
-        hw.add(new MyListItem("homework 1 : Calculator Activity ",calcActivity.class));
-        hw.add(new MyListItem("homework 2 : Birthday Activity ",calcActivity.class));
+        hw.add(new MyListItem("Calculator Activity ",calcActivity.class));
+        hw.add(new MyListItem("Birthday Activity ",birthdayActivity.class));
         //more homework..
     }
 
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener mClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Toast.makeText(getBaseContext(), "Activity is not attached", Toast.LENGTH_LONG).show();
+            Toast.makeText(getBaseContext(), "coming up", Toast.LENGTH_LONG).show();
             MyListItem clicked = (MyListItem) view.getTag();
             if (clicked.getActivity() == null) {
                 if (clicked.getPackageName() != null) {
@@ -87,32 +87,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-////        getMenuInflater().inflate(R.menu.lesson4, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        System.out.println("here onOptionsItemSelected");
-//        //noinspection SimplifiableIfStatement
-////        if (id == R.id.mi_settings) {
-////            Toast.makeText(getBaseContext(), "Todo: settings screen", Toast.LENGTH_LONG).show();
-////            return true;
-////        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//
-//    public void onPrefsClicked(View view) {
-////        startActivity(new Intent(this, PrefsActivity.class));
-//    }
+
 }
 
